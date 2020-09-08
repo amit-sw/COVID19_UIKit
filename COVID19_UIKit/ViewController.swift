99660e8d0d1d0334238c064af88b9b30f8cd32de
Amit Gupta <amit@gprof.com>
2020-09-08 12:49:43 -0700

    Initial Commit

---
+++ COVID19_UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19_UIKit
+//
+//  Created by Amit Gupta on 9/8/20.
+//  Copyright © 2020 AI Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
